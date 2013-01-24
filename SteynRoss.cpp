febd1cc097ac219d7c62e3257f81d3a587fa2312
Schellenberger <schellenberger@inb.uni-luebeck.de>
2013-01-24 12:26:05 +0100

    Added the possibility of an onset until data is stored, only in the mex file however

--- SteynRoss.cpp
+++ SteynRoss.cpp
@@ -12,7 +12,6 @@ using std::vector;
 // mex command is given by:
 // mex CXXFLAGS="\$CXXFLAGS -std=gnu++0x -fpermissive" SteynRoss.cpp Cortical_Colum.cpp
 
-extern const int T 		= 120;
 extern const int res 	= 1E3;
 extern const double dt 	= 1E3/res;
 extern const double h	= sqrt(dt);
@@ -25,11 +24,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	// Initializing the mersenne twister.
 	MTRand mtrand;
 
+	const int T		= (int)(mxGetScalar(prhs[0]));
+	const int onset	= (int)(mxGetScalar(prhs[1]));
+
+	const int Time 	= (T+onset)*res;
+
 	// creating the random input
-	vector<double> u_e1 = rand_var(T*res, mtrand, phi_sc, phi_sc);
-	vector<double> u_e2 = rand_var(T*res, mtrand, phi_sc, phi_sc);
-	vector<double> u_i1 = rand_var(T*res, mtrand, phi_sc, phi_sc);
-	vector<double> u_i2 = rand_var(T*res, mtrand, phi_sc, phi_sc);
+	vector<double> u_e1 = rand_var(Time, mtrand, phi_sc, phi_sc);
+	vector<double> u_e2 = rand_var(Time, mtrand, phi_sc, phi_sc);
+	vector<double> u_i1 = rand_var(Time, mtrand, phi_sc, phi_sc);
+	vector<double> u_i2 = rand_var(Time, mtrand, phi_sc, phi_sc);
 
 	// Initializing the populations;
 	Cortical_Colum Col;
@@ -38,11 +42,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	vector<double> Ve (T*res);
 	vector<double> Vi (T*res);
 
+	int count = 0;
+
 	// simulation
-	for (int t=0; t< T*res; ++t) {
+	for (int t=0; t<Time; ++t) {
 		ODE (Col, u_e1[t], u_e2[t], u_i1[t], u_i2[t]);
 		//ODE2(Col, u_e1[t], u_i1[t]);
-		get_data(t, Col, Ve, Vi);
+		if(t>=onset*res){
+		get_data(count, Col, Ve, Vi);
+		++count;
+		}
 	}
 
 	plhs[0] = getMexArray(Ve);

17e0bbaf3b2211b39b1a24c01016cddaea589b5e
Schellenberger <schellenberger@inb.uni-luebeck.de>
2014-04-24 15:16:47 +0200

    Code cleanups. Added license Made comments linebreak save
    Signed-off-by: Schellenberger <schellenberger@inb.uni-luebeck.de>

--- ODE.h
+++ ODE.h
@@ -1,3 +1,25 @@
+/*
+*	Copyright (c) 2014 Michael Schellenberger Costa
+*
+*	Permission is hereby granted, free of charge, to any person obtaining a copy
+*	of this software and associated documentation files (the "Software"), to deal
+*	in the Software without restriction, including without limitation the rights
+*	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*	copies of the Software, and to permit persons to whom the Software is
+*	furnished to do so, subject to the following conditions:
+*
+*	The above copyright notice and this permission notice shall be included in
+*	all copies or substantial portions of the Software.
+*
+*	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*	THE SOFTWARE.
+*/
+
 /****************************************************************************************************/
 /*									Implementation of the ODE solver								*/
 /****************************************************************************************************/
@@ -8,11 +30,12 @@
 /*										Evaluation of SRK4											*/
 /****************************************************************************************************/
 inline void ODE(Thalamic_Column& Thalamus) {
-	// first calculating every ith RK moment
-	// has to be in order, 1th moment first
+	/* First calculating every ith RK moment. Has to be in order, 1th moment first */
 	for (int i=1; i<=4; ++i) {
 		Thalamus.set_RK(i);
 	}
+
+	/* Add moments */
 	Thalamus.add_RK();
 }
 /****************************************************************************************************/
